06230b3fcbfe0aa60835b8d0095fe2054e1e9b6a
lein <171060003@qq.com>
2017-12-13 23:33:44 +0800

    run eureka server

--- zhuxiaoxiao-eureka/src/main/java/com/zhuxiaoxiao/eureka/ZhuxiaoxiaoEurekaApplication.java
+++ zhuxiaoxiao-eureka/src/main/java/com/zhuxiaoxiao/eureka/ZhuxiaoxiaoEurekaApplication.java
@@ -2,11 +2,18 @@ package com.zhuxiaoxiao.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ZhuxiaoxiaoEurekaApplication {
 
 	public static void main(String[] args) {
+//		SpringApplicationBuilder springApplicationBuilder =  new SpringApplicationBuilder(ZhuxiaoxiaoEurekaApplication.class);
+//		springApplicationBuilder.web(true);
+//		springApplicationBuilder.run(args);
+
 		SpringApplication.run(ZhuxiaoxiaoEurekaApplication.class, args);
 	}
 }
